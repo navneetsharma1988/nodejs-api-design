89caab35f5ffc2e16542ce426e4f0e59fa37a886
Navneet Sharma <navneet.sharma@admiral.at>
2023-03-26 21:44:56 +0530

    Install requisite dependencies, add route & auth handlers

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import { protect } from './modules/auth';
+import { createNewUser, signin } from './handlers/user';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  console.log('hello from express');
+  res.status(200);
+  res.json({ message: 'hello' });
+});
+
+app.use('/api', protect, router);
+
+app.use('/user', createNewUser);
+app.use('/signin', signin);
+
+export default app;
